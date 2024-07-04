8d72b9228a0dba3510c54f0f7d10f34e2f4d2a25
Leandro Poroli <leandro.poroli@kurtosistech.com>
2024-07-04 14:20:49 -0300

    adding SSL CERT configuration to containers

--- flake.nix
+++ flake.nix
@@ -158,6 +158,7 @@
                           container_pkgs.nettools
                           container_pkgs.gnugrep
                           container_pkgs.coreutils
+                          container_pkgs.cacert
                         ];
                         pathsToLink = ["/bin"];
                       };
@@ -166,6 +167,7 @@
                         if !needsCrossCompilation
                         then ["${service}/bin/${service.pname}"]
                         else ["${service}/bin/${os}_${arch}/${service.pname}"];
+                      config.Env = ["SSL_CERT_FILE=${container_pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"];
                     };
                 };
               }) {
