9130288531432bb04da6bcbc8533596c1eb02d64
lostbean <talktoedgar@gmail.com>
2024-07-09 11:56:37 -0300

    update install script

--- scripts/install_cli.sh
+++ scripts/install_cli.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+
+set -e
 
 REPO="kurtosis-tech/kardinal"
 BINARY_NAME="kardinal"
@@ -14,37 +16,48 @@ elif [[ "$ARCH" == "aarch64" ]]; then
 	ARCH="arm64"
 fi
 
-LATEST_RELEASE=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-
-DOWNLOAD_URL="https://github.com/$REPO/releases/download/$LATEST_RELEASE/${BINARY_NAME}-${OS}-${ARCH}"
-curl -L $DOWNLOAD_URL -o /tmp/${BINARY_NAME}
-
 BIN_FOLDER="$HOME/.local/bin"
 mkdir -p $BIN_FOLDER
 
-mv /tmp/$BINARY_NAME $BIN_FOLDER/$BINARY_NAME
+LATEST_RELEASE=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
+echo "Downloading $BINARY_NAME $LATEST_RELEASE for $OS $ARCH..."
+DOWNLOAD_URL="https://github.com/$REPO/releases/download/$LATEST_RELEASE/${BINARY_NAME}-${OS}-${ARCH}"
+curl -L $DOWNLOAD_URL -o /$BIN_FOLDER/$BINARY_NAME
+chmod +x $BIN_FOLDER/$BINARY_NAME
+
+USER_SHELL=$(basename $SHELL)
+echo "Detected shell: $USER_SHELL"
+
+handle_error() {
+	local exit_code=$?
+	echo "Ops! Failed to setup integration with your $USER_SHELL shell. Please add the following lines to 
+  your shell configuration manually (changes may not be persistent)
+export PATH=\$PATH:$BIN_FOLDER
+source <($BIN_FOLDER/$BINARY_NAME completion $USER_SHELL)"
+	exit $exit_code
+}
+
+trap 'handle_error' ERR
 
 if [ -f $BIN_FOLDER/$BINARY_NAME ]; then
-	if [ -d /usr/share/bash-completion/completions ]; then
-		source $($BIN_FOLDER/$BINARY_NAME completion bash)
+	if [ $USER_SHELL == 'bash' ]; then
+		echo "export PATH=\$PATH:$BIN_FOLDER" >>~/.bashrc
+		echo "source <($BIN_FOLDER/$BINARY_NAME completion bash)" >>~/.bashrc
+		source ~/.bashrc
 	fi
 
-	if [ -d ~/.zsh/completions ]; then
-		source $($BIN_FOLDER/$BINARY_NAME completion zsh)
+	if [ $USER_SHELL == 'zsh' ]; then
+		echo "export PATH=\$PATH:$BIN_FOLDER" >>~/.zshrc
+		echo "source <($BIN_FOLDER/$BINARY_NAME completion zsh)" >>~/.bashrc
+		source ~/.zshrc
 	fi
 
-	if [ -d ~/.config/fish/completions ]; then
-		source $($BIN_FOLDER/$BINARY_NAME completion fish)
+	if [ $USER_SHELL == 'fish' ]; then
+		echo "set -gx PATH \$PATH $BIN_FOLDER" >>~/.config/fish/config.fish
+		echo "source <($BIN_FOLDER/$BINARY_NAME completion fish)" >>~/.bashrc
+		source ~/.config/fish/config.fish
 	fi
 fi
 
 echo "$BINARY_NAME has been installed successfully!"
-
-if ! command -v $BINARY_NAME &>/dev/null; then
-	echo "export PATH=\$PATH:$BIN_FOLDER" >>~/.bashrc
-	source ~/.bashrc
-	echo "export PATH=\$PATH:$BIN_FOLDER" >>~/.zshrc
-	source ~/.zshrc
-	echo "set -gx PATH \$PATH $BIN_FOLDER" >>~/.config/fish/config.fish
-	source ~/.config/fish/config.fish
-fi
