6b4bcc11a7de5680cbb900217e35b01f71488945
lostbean <talktoedgar@gmail.com>
2024-08-06 10:02:46 -0300

    detect previous installs

--- scripts/install_cli.sh
+++ scripts/install_cli.sh
@@ -3,11 +3,11 @@ set -e
 
 # Function to handle errors
 handle_error() {
-    echo "Ops! Failed to setup integration with your shell. Please add the following lines to
+	echo "Ops! Failed to setup integration with your shell. Please add the following lines to
 your shell configuration manually (changes may not be persistent)
 export PATH=\$PATH:$BIN_FOLDER
 source <($BIN_FOLDER/$BINARY_NAME completion $PARENT_SHELL)"
-    exit 1
+	exit 1
 }
 
 # Rest of your script goes here
@@ -16,12 +16,18 @@ BINARY_NAME="kardinal"
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 case "$ARCH" in
-    x86_64) ARCH="amd64" ;;
-    arm64|aarch64) ARCH="arm64" ;;
+x86_64) ARCH="amd64" ;;
+arm64 | aarch64) ARCH="arm64" ;;
 esac
 
 BIN_FOLDER="$HOME/.local/bin"
 mkdir -p $BIN_FOLDER
+
+WAS_INTALLED_BEFORE=0
+if [ -f "$BIN_FOLDER/$BINARY_NAME" ]; then
+	WAS_INTALLED_BEFORE=1
+fi
+
 LATEST_RELEASE=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 echo "Downloading $BINARY_NAME $LATEST_RELEASE for $OS $ARCH..."
 DOWNLOAD_URL="https://github.com/$REPO/releases/download/$LATEST_RELEASE/${BINARY_NAME}-${OS}-${ARCH}"
@@ -32,38 +38,42 @@ PARENT_SHELL=$(ps -o comm= -p $PPID)
 echo "Detected parent shell: $PARENT_SHELL"
 
 if [ -f "$BIN_FOLDER/$BINARY_NAME" ]; then
-    case "$PARENT_SHELL" in
-        bash|bash)
-	    CONFIG_FILE="$HOME/.bashrc"
-            if ! echo "export PATH=\$PATH:$BIN_FOLDER" >> "$CONFIG_FILE" || \
-               ! echo "source <($BIN_FOLDER/$BINARY_NAME completion bash)" >> "$CONFIG_FILE"; then
-                handle_error
-            fi
-            ;;
-        zsh|zsh)
-	    CONFIG_FILE="$HOME/.zshrc"
-            if ! echo "export PATH=\$PATH:$BIN_FOLDER" >> "$CONFIG_FILE" || \
-               ! echo "autoload -U +X compinit && compinit" >> "$CONFIG_FILE" || \
-               ! echo "source <($BIN_FOLDER/$BINARY_NAME completion zsh)" >> "$CONFIG_FILE"; then
-                handle_error
-            fi
-            ;;
-        fish)
-	    CONFIG_FILE="$HOME/.config/fish/config.fish"
-            if ! echo "set -gx PATH \$PATH $BIN_FOLDER" >> "$CONFIG_FILE" || \
-               ! echo "source ($BIN_FOLDER/$BINARY_NAME completion fish | psub)" >> "$CONFIG_FILE"; then
-                handle_error
-            fi
-            ;;
-        *)
-            echo "Unrecognized shell: $PARENT_SHELL"
-            handle_error
-            ;;
-    esac
+	if [ $WAS_INTALLED_BEFORE -eq 0 ]; then
+		case "$PARENT_SHELL" in
+		bash | bash)
+			CONFIG_FILE="$HOME/.bashrc"
+			if ! echo "export PATH=\$PATH:$BIN_FOLDER" >>"$CONFIG_FILE" ||
+				! echo "source <($BIN_FOLDER/$BINARY_NAME completion bash)" >>"$CONFIG_FILE"; then
+				handle_error
+			fi
+			;;
+		zsh | zsh)
+			CONFIG_FILE="$HOME/.zshrc"
+			if ! echo "export PATH=\$PATH:$BIN_FOLDER" >>"$CONFIG_FILE" ||
+				! echo "autoload -U +X compinit && compinit" >>"$CONFIG_FILE" ||
+				! echo "source <($BIN_FOLDER/$BINARY_NAME completion zsh)" >>"$CONFIG_FILE"; then
+				handle_error
+			fi
+			;;
+		fish)
+			CONFIG_FILE="$HOME/.config/fish/config.fish"
+			if ! echo "set -gx PATH \$PATH $BIN_FOLDER" >>"$CONFIG_FILE" ||
+				! echo "source ($BIN_FOLDER/$BINARY_NAME completion fish | psub)" >>"$CONFIG_FILE"; then
+				handle_error
+			fi
+			;;
+		*)
+			echo "Unrecognized shell: $PARENT_SHELL"
+			handle_error
+			;;
+		esac
+		echo "$BINARY_NAME has been installed successfully!"
+		echo "Run the following command to load Kardinal in the current shell (new shell will already load it):"
+		echo "\n> source $CONFIG_FILE\n"
+	else
+		echo "Kardinal was installed before, just updated it."
+	fi
 else
-    handle_error
+	echo "Failed to install $BINARY_NAME. Please try again."
+	exit 1
 fi
-
-echo "$BINARY_NAME has been installed successfully!"
-echo "Run the following command to load Kardinal in the current shell (new shell will already load it):"
-echo "\n> source $CONFIG_FILE\n"
