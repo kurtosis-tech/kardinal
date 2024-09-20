253fe219e871981a296e2e884178d859294492a7
Laurent Luce <laurentluce49@yahoo.com>
2024-09-20 14:44:10 -0400

    fix: exit with code 1 upon errors in the CLI

--- kardinal-cli/main.go
+++ kardinal-cli/main.go
@@ -1,10 +1,16 @@
 package main
 
-import "kardinal.cli/cmd"
+import (
+	"fmt"
+	"os"
+
+	"kardinal.cli/cmd"
+)
 
 func main() {
 
 	if err := cmd.Execute(); err != nil {
-		println("Error:", err.Error())
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
 	}
 }
