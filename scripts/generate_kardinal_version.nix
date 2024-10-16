ad86c2cbfccdbc391eb93c092a3e1319a025fef9
Tedi Mitiku <tedi.m52@gmail.com>
2024-10-16 13:02:35 +0900

    fix nix again

--- scripts/generate_kardinal_version.nix
+++ scripts/generate_kardinal_version.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }:
 pkgs.writeShellApplication {
   name = "generate-kardinal-version";
-  runtimeInputs = with pkgs; [ git, get-docker-tag ];
+  runtimeInputs = with pkgs; [ git ];
 
   text = ''
     set -euo pipefail
     script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    root_dirpath="$(dirname "${script_dirpath}")"
+    root_dirpath="$(dirname "$script_dirpath")"
 
     KARDINAL_VERSION_PACKAGE_DIR="kardinal_version"
     KARDINAL_VERSION_GO_FILE="kardinal_version.go"
@@ -21,11 +21,11 @@ pkgs.writeShellApplication {
         exit 1
     }
 
-    new_version="${1:-}"
+    new_version=""
 
     if [ -z "$new_version" ]; then
         if ! cd "$root_dirpath"; then
-            echo "Error: Couldn't cd to the root of this repo, '${root_dirpath}', which is required to get the Git tag" >&2
+            echo "Error: Couldn't cd to the root of this repo, '$root_dirpath', which is required to get the Git tag" >&2
             show_helptext_and_exit
         fi
         if ! new_version="$(./scripts/get-docker-tag.sh)"; then
@@ -37,7 +37,7 @@ pkgs.writeShellApplication {
     kardinal_version_go_file_abs_path="$root_dirpath/$KARDINAL_VERSION_PACKAGE_DIR/$KARDINAL_VERSION_GO_FILE"
 
     cat << EOF > "$kardinal_version_go_file_abs_path"
-package ${KARDINAL_VERSION_PACKAGE_DIR}
+package $KARDINAL_VERSION_PACKAGE_DIR
 
 const (
     // !!!!!!!!!!!!!!!!!! DO NOT MODIFY THIS! IT WILL BE UPDATED AUTOMATICALLY DURING THE BUILD PROCESS !!!!!!!!!!!!!!!
