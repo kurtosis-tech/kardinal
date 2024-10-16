0f3d297e6558ae619e11627a38762040c6a6bfe2
Tedi Mitiku <tedi.m52@gmail.com>
2024-10-16 23:22:05 +0900

    call expression before building cli golang app

--- kardinal-cli/default.nix
+++ kardinal-cli/default.nix
@@ -1,47 +1,28 @@
-{ pkgs, commit_hash ? "dirty" }: let
+{
+    pkgs,
+    commit_hash ? "dirty"
+}: let
   pname = "kardinal.cli";
 
-  # Use the generate-kardinal-version Nix expression
   generateKardinalVersion = pkgs.callPackage ./scripts/generate_kardinal_version.nix {
     inherit pkgs;
+  };
+  _ = pkgs.runCommand "generate-version" {
+    buildInputs = [ generateKardinalVersion ];
   };
 
   ldflags = pkgs.lib.concatStringsSep "\n" [
     "-X github.com/kurtosis-tech/kurtosis/kardinal.AppName=${pname}"
     "-X github.com/kurtosis-tech/kurtosis/kardinal.Commit=${commit_hash}"
   ];
-
 in
-  pkgs.stdenv.mkDerivation {
-    name = pname;
+  pkgs.buildGoApplication {
+    # pname has to match the location (folder) where the main function is or use
+    # subPackages to specify the file (e.g. subPackages = ["some/folder/main.go"];)
+    inherit pname ldflags;
+    name = "${pname}";
+    pwd = ./.;
     src = ./.;
-    buildInputs = [ generateKardinalVersion ] ++ (with pkgs; [
-      go
-      # Add any other dependencies your build needs
-    ]);
-
-    phases = [ "buildPhase" "installPhase" ]; # Ensure buildPhase is included
-
-    buildPhase = ''
-      # Run the version generation before building
-      ${generateKardinalVersion}/bin/generate-kardinal-version
-
-      # Now build the Go application
-      ${pkgs.go}/bin/go build -ldflags "${ldflags}" -o ${pname} ./...
-    '';
-
-    installPhase = ''
-      # Install the built binary
-      mkdir -p $out/bin
-      mv ${pname} $out/bin/
-    '';
-
-    # This will ensure that the generated file is part of the final output
-    installCheckPhase = ''
-      # Check if the version file exists and is generated correctly
-      if [ ! -f "kardinal_version/kardinal_version.go" ]; then
-        echo "Version file not generated!"
-        exit 1
-      fi
-    '';
+    modules = ./gomod2nix.toml;
+    CGO_ENABLED = 0;
   }
