a8676956a5f8e772e818c0cac0ac35fb61eb6799
Tedi Mitiku <tedi.m52@gmail.com>
2024-10-16 12:55:19 +0900

    fix get docker tag

--- scripts/get-docker-tag.nix
+++ scripts/get-docker-tag.nix
@@ -13,6 +13,6 @@ pkgs.writeShellApplication {
 
     commit_sha="$(git rev-parse --short=6 HEAD)"
     suffix="$(git diff --quiet || echo '-dirty')"
-    echo "${commit_sha}${suffix}"
+    echo "$commit_sha$suffix"
   '';
 }
